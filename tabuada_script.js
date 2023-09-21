d309c502fa4b516ae49c75273c270587d44e4437
Lucas Vidal <Vidal.lucas94@gmail.com>
2023-09-21 12:54:26 -0300

    Versao Inicial

---
+++ tabuada_script.js
@@ -0,0 +1,22 @@
+ function Tabuada () {
+var num = window.document.getElementById ('num')
+var end = document.getElementById ('end')
+
+
+    if (num.value == 0 || end.value == 0){
+    window.alert ('[ERRO] Verifique seus dados!')
+} else {
+var n = Number (num.value)
+var e = Number (end.value)
+var x = 0
+var r = ''
+var res= window.document.getElementById ('resposta')
+res.innerHTML = `A tabuada do ${n} é: <br>`
+ do {
+    r= n*x
+    res.innerHTML += `${x} X ${n} = ${r} <br>`
+    x++
+ } while ( x<= e )
+
+}
+ }
